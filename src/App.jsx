2d1de5ff8532eedc1e76f3d4d7a8ad11102144ba
Stephan Granzow <stephan_granzow@web.de>
2024-05-20 19:43:54 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import StudentDetailsPage from "./components/StudentDetailsPage";
+import UserProfilePage from "./components/UserProfilePage";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import StudentDetailsPage from "./pages/StudentDetailsPage";
-import UserProfilePage from "./pages/UserProfilePage";
-
 
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
+    <div className="App">
       <Navbar />
-
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
     </div>
   );
 }
